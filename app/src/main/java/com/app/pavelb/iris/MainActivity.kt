ba92708b63e2e6d0bd8a280ad5fd87d0d097c5d2
pavel-borik <pavel.borik1@gmail.com>
2018-11-04 15:22:57 +0100

    Init

---
+++ app/src/main/java/com/app/pavelb/iris/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.app.pavelb.iris
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mainStartButton.setOnClickListener {
+            val i = Intent(this, RecognitionActivity::class.java)
+            startActivity(i)
+        }
+    }
+}
---
+++ app/src/main/java/com/app/pavelb/iris/RecognitionActivity.kt
@@ -0,0 +1,130 @@
+package com.app.pavelb.iris
+
+import android.os.Bundle
+import org.opencv.android.CameraBridgeViewBase
+import org.opencv.imgproc.Imgproc
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame
+import org.opencv.android.LoaderCallbackInterface
+import org.opencv.android.OpenCVLoader
+import org.opencv.android.BaseLoaderCallback
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2
+import android.view.View.OnTouchListener
+import android.app.Activity
+import android.util.Log
+import android.view.*
+import org.opencv.core.*
+
+class RecognitionActivity : Activity(), OnTouchListener, CvCameraViewListener2 {
+
+    private var mIsColorSelected = false
+    private var mRgba: Mat? = null
+    private var mBlobColorRgba: Scalar? = null
+    private var mBlobColorHsv: Scalar? = null
+    private var mSpectrum: Mat? = null
+    private var SPECTRUM_SIZE: Size? = null
+    private var CONTOUR_COLOR: Scalar? = null
+
+    private var mOpenCvCameraView: CameraBridgeViewBase? = null
+
+    private val mLoaderCallback = object : BaseLoaderCallback(this) {
+        override fun onManagerConnected(status: Int) {
+            when (status) {
+                LoaderCallbackInterface.SUCCESS -> {
+                    Log.i(TAG, "OpenCV loaded successfully")
+                    mOpenCvCameraView!!.enableView()
+                    mOpenCvCameraView!!.setOnTouchListener(this@RecognitionActivity)
+                }
+                else -> {
+                    super.onManagerConnected(status)
+                }
+            }
+        }
+    }
+
+    init {
+        Log.i(TAG, "Instantiated new " + this.javaClass)
+    }
+
+    /** Called when the activity is first created.  */
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i(TAG, "called onCreate")
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        setContentView(R.layout.activity_recognition)
+
+        mOpenCvCameraView = findViewById(R.id.cameraSurfaceView)
+        mOpenCvCameraView!!.visibility = SurfaceView.VISIBLE
+        mOpenCvCameraView!!.setCvCameraViewListener(this)
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView!!.disableView()
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization")
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback)
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!")
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS)
+        }
+    }
+
+    public override fun onDestroy() {
+        super.onDestroy()
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView!!.disableView()
+    }
+
+    override fun onCameraViewStarted(width: Int, height: Int) {
+        mRgba = Mat(height, width, CvType.CV_8UC4)
+        mSpectrum = Mat()
+        mBlobColorRgba = Scalar(255.0)
+        mBlobColorHsv = Scalar(255.0)
+        SPECTRUM_SIZE = Size(200.0, 64.0)
+        CONTOUR_COLOR = Scalar(255.0, 0.0, 0.0, 255.0)
+    }
+
+    override fun onCameraViewStopped() {
+        mRgba!!.release()
+    }
+
+    override fun onTouch(v: View, event: MotionEvent): Boolean {
+        val cols = mRgba!!.cols()
+        val rows = mRgba!!.rows()
+
+        val xOffset = (mOpenCvCameraView!!.width - cols) / 2
+        val yOffset = (mOpenCvCameraView!!.height - rows) / 2
+
+        val x = event.x.toInt() - xOffset
+        val y = event.y.toInt() - yOffset
+
+        Log.i(TAG, "Touch image coordinates: ($x, $y)")
+
+        return false // don't need subsequent touch events
+    }
+
+    override fun onCameraFrame(inputFrame: CvCameraViewFrame): Mat? {
+        mRgba = inputFrame.rgba()
+
+        return mRgba
+    }
+
+    private fun converScalarHsv2Rgba(hsvColor: Scalar?): Scalar {
+        val pointMatRgba = Mat()
+        val pointMatHsv = Mat(1, 1, CvType.CV_8UC3, hsvColor!!)
+        Imgproc.cvtColor(pointMatHsv, pointMatRgba, Imgproc.COLOR_HSV2RGB_FULL, 4)
+
+        return Scalar(pointMatRgba.get(0, 0))
+    }
+
+    companion object {
+        private val TAG = "RecognitionActivity"
+    }
+}
